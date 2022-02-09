1fcbfdd926282cd4e410b8a59d2169d5bc07b431
Brett Howard <howardcompositions@gmail.com>
2022-02-09 11:39:48 -0700

    Started Queries

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -1,12 +1,81 @@
 import { gql } from '@apollo/client';
 
-export const QUERY_BOOKS = gql`
-  query Posts {
-    posts {
-      title
-      user {
-        name
+export const QUERY_POSTS = gql`
+  query posts($PostedBy: String) {
+    posts(PostedBy: $PostedBy) {
+      _id
+      PostBody
+      createdAt
+      Comments {
+        _id
+        CommentText
+        CommentedBy
+        createdAt
       }
     }
   }
+`;
+
+export const QUERY_POST = gql`
+  query post($id: ID!) {
+    post(_id: $id) {
+      _id
+      title
+      PostBody
+      createdAt
+      Comment {
+        _id
+        CommentText
+        CommentedBy
+        createdAt
+        replies {
+          ReplyText
+          RepliedBy
+          createdAt
+        }
+      }
+    }
+  }
+`;
+
+export const QUERY_USER = gql`
+  query user($name: String!) {
+    user(name: $name) {
+      _id
+      name
+      email
+      posts {
+        _id
+        title
+        PostBody
+        PostedBy
+        Comments
+        createdAt
+      }
+    }
+  }
+`;
+
+export const QUERY_COMMENT = gql`
+query comment($CommentedBy: String!) {
+  reply(CommentedBy: $CommentedBy) {
+    _id
+    CommentText
+    Replies: {
+      ReplyText
+      RepliedBy
+      createdAt
+    }
+  }
+}
+`;
+
+export const QUERY_REPLY = gql `
+query reply($RepliedBy: String!) {
+  reply(RepliedBy: $RepliedBy) {
+    _id
+    ReplyText
+    createdAt
+  }
+}
 `;
