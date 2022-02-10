513e2a05f1c74de68d7ea8926ad6bec9c2b7232d
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2022-02-09 18:49:52 -0700

    fixing form

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,9 +4,11 @@ import { LOGIN_MUTATION } from '../graphql/mutations';
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ username: '', password: '' });
-  const [username, setUsername] = useState('');
+  const [loginUsername, setLoginUsername] = useState('');
+  const [signUpUsername, setSignUpUsername] = useState('');
   // loginmutation had loading and error in the {} but they were throwing errors so I removed them for the time being 
-  const [login, { data }] = useMutation(LOGIN_MUTATION);
+  const [login, { loginData }] = useMutation(LOGIN_MUTATION);
+  const [addUser, { signUpData }] = useMutation(LOGIN_MUTATION);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -19,10 +21,10 @@ const Login = (props) => {
   };
 
   useEffect((props) => {
-    if (data && data.login) {
-      props.setUser({ token: data.login.token, user: data.login.user });
+    if (loginData && loginData.login) {
+      props.setUser({ token: loginData.login.token, user: loginData.login.user });
     }
-  }, [data]);
+  }, [loginData]);
   
   return (
     <div>
@@ -47,6 +49,41 @@ const Login = (props) => {
         e.preventDefault();
         login({ variables: { name: username } });
       }}>
+              <h3 className='form-title'>Login</h3>
+              <label htmlFor="login-username">
+                Username:
+              </label>
+              <input
+                className="form-input"
+                placeholder="Your Username"
+                name="login-username"
+                type="text"
+                id="login-username"
+                value={formState.username}
+                onChange={handleChange}
+              />
+              <label htmlFor="login-password">
+                Password:
+              </label>
+              <input
+                className="form-input"
+                placeholder="******"
+                name="login-password"
+                type="password"
+                id="login-password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button className="btn d-block w-100" type="submit">
+                Login
+              </button>
+      </form>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        addUser({ variables: { name: username } });
+      }}>
+              <h3 className='form-title'>Sign-Up</h3>
               <label htmlFor="username">
                 Username:
               </label>
@@ -57,6 +94,18 @@ const Login = (props) => {
                 type="text"
                 id="username"
                 value={formState.username}
+                onChange={handleChange}
+              />
+              <label htmlFor="email">
+                Email:
+              </label>
+              <input
+                className="form-input"
+                placeholder="Your Email"
+                name="email"
+                type="text"
+                id="email"
+                value={formState.email}
                 onChange={handleChange}
               />
               <label htmlFor="password">
@@ -72,9 +121,9 @@ const Login = (props) => {
                 onChange={handleChange}
               />
               <button className="btn d-block w-100" type="submit">
-                Login
+                SignUp
               </button>
-            </form>
+      </form>
 
       
     </div>
