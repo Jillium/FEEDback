7228b004c05d766742cd897a5f24e6ff9dcd49fc
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-14 18:58:36 -0700

    progress save

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { QUERY_POSTS } from "../graphql/queries";
+import { QUERY_POSTS, QUERY_ME_BASIC } from "../graphql/queries";
 import PostList from '../components/PostList';
+
+
 
 function Home() {
 
     const { loading, data } = useQuery(QUERY_POSTS);
+    const { data: userData } = useQuery(QUERY_ME_BASIC);
+    console.log(data);
 
     const posts = data?.posts || [];
     console.log(posts);
@@ -13,9 +17,13 @@ function Home() {
 
     return (
         <main>
-            <div>
-                <div><PostList posts={posts}/></div>
-            </div>
+           <div>
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <PostList posts={posts} />
+          )}
+        </div>
         </main>
     );
 
