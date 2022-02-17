874b8fa9050c24986cdf78f0c2c0114c73e76c98
Jillium <86424914+Jillium@users.noreply.github.com>
2022-02-17 13:24:28 -0700

    working on comments, not fully displaying in singlepost yet. Also fixed auth so secret is a secret

--- client/src/graphql/queries.js
+++ client/src/graphql/queries.js
@@ -9,10 +9,12 @@ export const QUERY_POSTS = gql`
       title
       postLink
       createdAt
+      commentCount
       comments {
         _id
         commentText
         createdAt
+        username
       }
     }
   }
