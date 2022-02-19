2fd082b30c8c17f2ce3b7c28867226c4373f0506
pc611652003 <88979797+pc611652003@users.noreply.github.com>
2022-02-19 12:03:55 -0700

    debug QUERY-USER

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,7 +7,7 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async ( parent,args, context) => {
+    me: async ( parent, args, context) => {
       if (context.user) {
         const userData = await User.findOne({ _id: context.user._id })
           .select('-__V -password')
