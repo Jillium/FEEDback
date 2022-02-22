f8b36b13c6e5699c100a71b96939e038c70fadc8
Travis Lloyd <travis.t.lloyd1996@gmail.com>
2022-02-22 16:36:36 -0700

    addes styling

--- client/src/pages/SinglePost.jsx
+++ client/src/pages/SinglePost.jsx
@@ -52,16 +52,17 @@ const SinglePost = props => {
                 </p>
 
                 <div>
-                    <p className='comment-p'>
-                        {post.commentCount > 0 && <CommentList comments={post.comments} />}
-                    </p>
-                </div>
-                <div>
                     <LinkPreview url={post.postLink} width='300px' height='300px' fallbackImageSrc='https://live.staticflickr.com/3238/3039847767_826d72d7a5_c.jpg' />
                 </div>
 
                 <div>
-                    <button className="btn ml-auto" onClick={handleClick}>Delete This Post</button>
+                    <button className="btn btn-secondary ml-auto m-1" onClick={handleClick}>Delete This Post</button>
+                </div>
+
+                <div>
+                    <p className='comment-p'>
+                        {post.commentCount > 0 && <CommentList comments={post.comments} />}
+                    </p>
                 </div>
 
                 <div>
